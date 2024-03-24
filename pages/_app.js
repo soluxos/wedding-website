5eb5c926a29ea5571eb005311f83c37eb4dcf06d
Callum Harrod <callumharrod1994@hotmail.co.uk>
2024-03-24 16:55:32 +0000

    first commit

---
+++ pages/_app.js
@@ -0,0 +1,28 @@
+import "../styles/globals.css";
+import { storyblokInit, apiPlugin } from "@storyblok/react";
+import Feature from "../components/Feature";
+import Grid from "../components/Grid";
+import Page from "../components/Page";
+import Teaser from "../components/Teaser";
+
+const components = {
+  feature: Feature,
+  grid: Grid,
+  teaser: Teaser,
+  page: Page,
+};
+
+storyblokInit({
+  accessToken: "urjCBREf6OrLpC3oWCrPqQtt",
+  use: [apiPlugin],
+  components,
+  apiOptions: {
+    region: ''
+  }
+});
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;

0dcd620a7dc924b07e07f39e21e7f4b950b3c7cd
Callum Harrod <callumharrod1994@hotmail.co.uk>
2024-09-09 18:09:08 +0100

    Added some better mobile styling

--- components/templates/Page.js
+++ components/templates/Page.js
@@ -70,6 +70,8 @@ const StyledBanner = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  padding: 10px;
+  text-align: center;
 
   p {
     color: ${({ theme }) => theme.colors.background};
